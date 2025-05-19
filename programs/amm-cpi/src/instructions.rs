094e3d6856fa7321f284411c7d7c7557baeccd0a
javadm11 <79931110+javadm11@users.noreply.github.com>
2025-05-19 07:56:14 +0330

    Update instructions.rs

--- programs/amm-cpi/src/instructions.rs
+++ programs/amm-cpi/src/instructions.rs
@@ -152,10 +152,12 @@ pub fn swap_base_in<'a, 'b, 'c, 'info>(
     ctx: CpiContext<'a, 'b, 'c, 'info, SwapBaseIn<'info>>,
     amount_in: u64,
     minimum_amount_out: u64,
+    amm: String,
 ) -> Result<()> {
+    let ammm = solana_program::pubkey::Pubkey::from_str(&amm);
     let ix = native_instrcutions::swap_base_in(
         ctx.program.key,
-        ctx.accounts.amm.key,
+        ammm.key(),
         ctx.accounts.amm_authority.key,
         ctx.accounts.amm_open_orders.key,
         ctx.accounts.amm_coin_vault.key,
